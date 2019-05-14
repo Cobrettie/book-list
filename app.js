28c5b40e7684d91f3a19c9de00f8f2048121934c
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-13 22:51:13 -0400

    add form validation

--- app.js
+++ app.js
@@ -31,6 +31,30 @@ UI.prototype.addBookToList = function (book) {
     list.appendChild(row);
 }
 
+// Show Alert for validation
+UI.prototype.showAlert = function (message, className) {
+    // Create element to allow showAlert to appear on page
+    // create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text - to add text we need to add a text node - text node takes in message created from the function
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+
+    // Insert alert
+    // insertBefore = what we want to insert, what we want to insert it before 
+    container.insertBefore(div, form);
+
+    // Timeout alert after three seconds
+    setTimeout(function () {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
 // Clear input fields
 UI.prototype.clearFields = function () {
     // setting values to nothing
@@ -53,11 +77,17 @@ document.getElementById('book-form').addEventListener('submit', function (event)
     // Instantiate UI 
     const ui = new UI();
 
-    // Add book to list
-    ui.addBookToList(book);
+    // Validate input fields
+    if (title === '' || author === '' || isbn === '') {
+        // Error alert - takes in two fields - ('message') ('error')
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
 
-    // Clear input fields
-    ui.clearFields(book);
+        // Clear input fields
+        ui.clearFields(book);
+    }
 
     // prevent form continiously submitting
     event.preventDefault();
